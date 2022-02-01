15c5243eac708fb45a095f8f246e190c60fe84a4
Mostafa Sayed Abdelmonam <43078536+eng-mostafa-sayed@users.noreply.github.com>
2022-02-01 20:10:01 +0200

    Update dishes.js

--- models/dishes.js
+++ models/dishes.js
@@ -1,10 +1,35 @@
 const Mongoose = require("mongoose");
 const Schema = Mongoose.Schema;
 
+//adding another schema into my schema (relation)
+
+const commentSchema = new Schema({
+    taring: {
+        type: Number,
+        min: 1,
+        max: 5,
+        required: true,
+    },
+    comment: {
+        type: String,
+        required: true,
+    },
+    author: {
+        type: String,
+        required: true,
+    },
+}, {
+    timestamps: true,
+});
+
 const dishSchema = new Schema({
         name: { type: String, required: true, unique: true },
         description: { type: String, required: true, unique: false },
-    }, //end of the first schema element, then the time stamp
+        //each documment in dishes has one or many comment (array of comments)
+        comments: [commentSchema],
+    },
+
+    //end of the first schema element, then the time stamp
     {
         timestamps: true,
     }
